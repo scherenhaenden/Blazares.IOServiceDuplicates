43e17fc204364c20d32a592b84ccc4f0bdec4a22
Eddie Krill <scherenhaenden@gmail.com>
2021-01-01 16:34:55 +0100

    Implementate models generator

--- Blazares.IOServiceDuplicates.Engine/Models/GenerateFiles.cs
+++ Blazares.IOServiceDuplicates.Engine/Models/GenerateFiles.cs
@@ -1,4 +1,26 @@
-$HEADER$namespace $NAMESPACE$
+using System.Collections.Generic;
+
+namespace Blazares.IOServiceDuplicates.Engine.Models
 {
-  public class $CLASS$ {$END$}
+    public class GenerateFiles
+    {
+        public GenerateFiles ()
+        {
+        }
+
+        public List<FileModel> StringsToModels(string [] Files)
+        {
+            List<FileModel> AllFiles = new List<FileModel> ();
+            FileModel TempFile;
+            foreach(var filestring in Files)
+            {
+				
+                TempFile = new FileModel ();
+                TempFile.FullPathOfFile = filestring;
+                AllFiles.Add (TempFile);
+            }
+            return AllFiles;
+        }
+
+    }
 }
