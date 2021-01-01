5ec35079b188ea9b9f5ffe0b88bd72906f8d6ad8
Eddie Krill <scherenhaenden@gmail.com>
2021-01-01 19:19:37 +0100

    Add first class for testing

---
+++ Blazares.IOServiceDuplicates.Engine.Tests/Engine/SetupClass.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace Blazares.IOServiceDuplicates.Engine.Tests.Engine
+{
+    
+    public class SetupClass
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+    }
+}
